1d35cbf209c9af7b8fbc758c2804a95f75a8f7eb
md-nasim-mondal <nasim.programmer24@gmail.com>
2026-01-29 00:01:48 +0600

    add vercel config file

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,8 +4,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
